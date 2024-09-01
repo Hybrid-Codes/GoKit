5dab4622b58f30f988a3e280010e4e2e48e361e6
Robert Burston <robbieburston@gmail.com>
2024-09-01 12:41:59 +0200

    Add login and signup handlers

---
+++ Backend/handlers/login.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+    "encoding/json"
+    "net/http"
+	"github.com/Hybrid-Codes/GoKit/models"
+    "golang.org/x/crypto/bcrypt"
+    "gorm.io/gorm"
+)
+
+type LoginInput struct {
+    Email    string `json:"email"`
+    Password string `json:"password"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+    var input LoginInput
+    if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    var user models.User
+    if err := models.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+        if err == gorm.ErrRecordNotFound {
+            http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+        } else {
+            http.Error(w, "Internal server error", http.StatusInternalServerError)
+        }
+        return
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+        http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(user)
+}
