916f5ca008ec6c482194fa8775acb90d6269cc37
Robert Burston <robbieburston@gmail.com>
2024-09-01 12:42:37 +0200

    Add post request to test API
    also imported handlers

--- Backend/main.go
+++ Backend/main.go
@@ -4,6 +4,7 @@ import (
     "log"
     "net/http"
     "github.com/Hybrid-Codes/GoKit/models"
+    "github.com/Hybrid-Codes/GoKit/handlers"
     "github.com/gorilla/mux"
 )
 
@@ -15,6 +16,8 @@ func main() {
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello, World!"))
     })
+    r.HandleFunc("/signup", handlers.SignupHandler).Methods("POST")
+    r.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
 
     log.Println("Starting server on :8080")
     if err := http.ListenAndServe(":8080", r); err != nil {
