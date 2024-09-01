0763c8a7406fd7021d429d6ee7f3897a43f31b60
Robert Burston <robbieburston@gmail.com>
2024-09-01 12:41:59 +0200

    Add login and signup handlers

---
+++ Backend/handlers/signup.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+    "encoding/json"
+    "net/http"
+    "github.com/Hybrid-Codes/GoKit/models"
+    "golang.org/x/crypto/bcrypt"
+)
+
+type SignupInput struct {
+    Name     string `json:"name"`
+    Email    string `json:"email"`
+    Password string `json:"password"`
+}
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+    var input SignupInput
+    if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+    if err != nil {
+        http.Error(w, "Error hashing password", http.StatusInternalServerError)
+        return
+    }
+
+    user := models.User{
+        Name:     input.Name,
+        Email:    input.Email,
+        Password: string(hashedPassword),
+    }
+
+    if err := models.DB.Create(&user).Error; err != nil {
+        http.Error(w, "Error creating user", http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(user)
+}
